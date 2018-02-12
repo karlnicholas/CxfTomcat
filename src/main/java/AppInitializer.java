5d73637bc5ec81113ecc90d849dbd2bb0533ac8d
Karl Nicholas <karl.nicholas@outlook.com>
2018-02-12 15:26:49 -0700

    remove extra indentation

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -8,19 +8,19 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 
-    public class AppInitializer implements WebApplicationInitializer {
-        @Override
-        public void onStartup(ServletContext container) {
-    
-            AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-            context.register(ServiceConfiguration.class);
-    
-            container.addListener(new ContextLoaderListener(context));
-    
-            // Register and map the dispatcher servlet
-            ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new CXFServlet());
-            dispatcher.setLoadOnStartup(1);
-    
-            dispatcher.addMapping("/services/*");
-        }
+public class AppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) {
+
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(ServiceConfiguration.class);
+
+        container.addListener(new ContextLoaderListener(context));
+
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new CXFServlet());
+        dispatcher.setLoadOnStartup(1);
+
+        dispatcher.addMapping("/services/*");
     }
+}
--- src/main/java/ServiceConfiguration.java
+++ src/main/java/ServiceConfiguration.java
@@ -5,19 +5,19 @@ import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-    @Configuration
-    public class ServiceConfiguration {
-    
-        @Bean
-        public Endpoint endpoint() {
-            EndpointImpl endpoint = new EndpointImpl(cxf(), new HelloWorldImpl());
-            endpoint.publish("/HelloWorld");
-            return endpoint;
-        }
-        
-        @Bean
-        public SpringBus cxf() {
-            return new SpringBus();
-        }
-        
+@Configuration
+public class ServiceConfiguration {
+
+    @Bean
+    public Endpoint endpoint() {
+        EndpointImpl endpoint = new EndpointImpl(cxf(), new HelloWorldImpl());
+        endpoint.publish("/HelloWorld");
+        return endpoint;
     }
+    
+    @Bean
+    public SpringBus cxf() {
+        return new SpringBus();
+    }
+    
+}
